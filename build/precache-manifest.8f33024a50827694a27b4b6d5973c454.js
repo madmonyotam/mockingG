5848615fb1e74c3dbb3f03a5c5586b687e683dfd
madmonyotam <madmonyotam@gmail.com>
2019-11-10 15:53:18 +0200

    add new build

---
+++ build/precache-manifest.8f33024a50827694a27b4b6d5973c454.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6410c3fdc38cb290ac132e76b01cddb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "851285952449639f8a95",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "eecaa18175772dbb9962",
+    "url": "/static/js/2.9b18cc33.chunk.js"
+  },
+  {
+    "revision": "851285952449639f8a95",
+    "url": "/static/js/main.a44c9944.chunk.js"
+  },
+  {
+    "revision": "4f77a4deb91dda449a06",
+    "url": "/static/js/runtime-main.124e7e57.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57e112f233575a735c63ad9090858950.js"
+  "/precache-manifest.8f33024a50827694a27b4b6d5973c454.js"
 );
 
 self.addEventListener('message', (event) => {

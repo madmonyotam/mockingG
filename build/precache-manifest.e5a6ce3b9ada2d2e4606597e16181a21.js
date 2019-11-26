adea43e6546deae3f0ba956a69b5cfd19a7c255f
madmonyotam <madmonyotam@gmail.com>
2019-11-26 08:22:52 +0200

    new build

---
+++ build/precache-manifest.e5a6ce3b9ada2d2e4606597e16181a21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fd965a0e4b7ecfef8b6fa3dc94926d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e16733c2f98334cfdfbb",
+    "url": "/static/css/main.9cff8ac4.chunk.css"
+  },
+  {
+    "revision": "9014f7c4d6b71d3255bf",
+    "url": "/static/js/2.ac7528cb.chunk.js"
+  },
+  {
+    "revision": "e16733c2f98334cfdfbb",
+    "url": "/static/js/main.593df8db.chunk.js"
+  },
+  {
+    "revision": "4f77a4deb91dda449a06",
+    "url": "/static/js/runtime-main.124e7e57.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c81152709d3496c3e8bea1e944fd29ac.js"
+  "/precache-manifest.e5a6ce3b9ada2d2e4606597e16181a21.js"
 );
 
 self.addEventListener('message', (event) => {

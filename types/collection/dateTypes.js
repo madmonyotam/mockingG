0355667a7f274e92deea8b1cba9e2db92a89971f
madmonyotam <madmonyotam@gmail.com>
2019-12-10 11:14:15 +0200

    date types - improved

--- types/collection/dateTypes.js
+++ types/collection/dateTypes.js
@@ -1,50 +1,97 @@
-const faker = require('faker');
-const moment = require('moment');
-
+const faker = require("faker");
+const moment = require("moment");
+const translate = require("../../translate/getTranslate");
 
 const dateTypes = {
-    month : {
-        name: "month",
-        generate: (el)=>{
-            return faker.date.month();
-        },
-        group: 'date',
+  month: {
+    name: "month",
+    generate: el => {
+      return faker.date.month();
     },
-    weekday : {
-        name: "weekday",
-        generate: (el)=>{
-            return faker.date.weekday();
-        },
-        group: 'date',
+    group: "date"
+  },
+  weekday: {
+    name: "weekday",
+    generate: el => {
+      return faker.date.weekday();
     },
-    futureDate : {
-        name: "future date",
-        renderer: {
-            dateMask: {
-                type: "string",
-                placeholder: 'DD-MM-YYYY'
-            }
-        },
-        generate: (el)=>{
-            let dateformat = el.value && el.value.dateMask || null;
-            return moment(faker.date.future( 50 )).format(dateformat);
-        },
-        group: 'date',
+    group: "date"
+  },
+  recentDate: {
+    name: "recent date",
+    renderer: {
+      dateMask: {
+        type: "string",
+        placeholder: "DD-MM-YYYY"
+      },
+      days: {
+        type: "number",
+        placeholder: "last X days"
+      }
     },
-    pastDate : {
-        name: "past date",
-        renderer: {
-            dateMask: {
-                type: "string",
-                placeholder: 'DD-MM-YYYY'
-            }
-        },
-        generate: (el)=>{
-            let dateformat = el.value && el.value.dateMask || null;
-            return moment(faker.date.past( 50 )).format(dateformat);
-        },
-        group: 'date',
+    generate: el => {
+      const { value } = el;
+      if (!value) return translate("missing value");
+      let dateformat = value.dateMask || null;
+
+      if (!value.days) return translate("missing days property");
+      const days = Number(value.days);
+
+      if (isNaN(days)) return translate("days value is not a number");
+      return moment(faker.date.recent(days)).format(dateformat);
     },
+    group: "date"
+  },
+  futureDate: {
+    name: "future date",
+    renderer: {
+      dateMask: {
+        type: "string",
+        placeholder: "DD-MM-YYYY"
+      },
+      years: {
+        type: "number",
+        placeholder: "last X years"
+      }
+    },
+    generate: el => {
+      const { value } = el;
+      if (!value) return translate("missing value");
+      let dateformat = el.value.dateMask || null;
+
+      if (!value.years) return translate("missing years property");
+      const years = Number(value.years);
+
+      if (isNaN(years)) return translate("years value is not a number");
+      return moment(faker.date.future(years)).format(dateformat);
+    },
+    group: "date"
+  },
+  pastDate: {
+    name: "past date",
+    renderer: {
+      dateMask: {
+        type: "string",
+        placeholder: "DD-MM-YYYY"
+      },
+      years: {
+        type: "number",
+        placeholder: "last X years"
+      }
+    },
+    generate: el => {
+      const { value } = el;
+      if (!value) return translate("missing value");
+      let dateformat = (el.value && el.value.dateMask) || null;
+
+      if (!value.years) return translate("missing years property");
+      const years = Number(value.years);
+
+      if (isNaN(years)) return translate("years value is not a number");
+      return moment(faker.date.past(years)).format(dateformat);
+    },
+    group: "date"
+  }
 };
 
 module.exports = dateTypes;
